9501395566dd7c29b8891162f966eb95fbf2a72f
Isaiah Eusebio <isaiahc.eusebio@gmail.com>
2025-11-04 17:08:50 -0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">Recipe Tracker</h1>
+          <Link to={"/"} className="text-4xl font-bold text-primary font-sans tracking-tighter">Recipe Tracker</Link>
           <div className="flex items-center gap=4">
             <Link to={"/create"} className="btn btn-primary">
               <PlusIcon className="size-5" />
