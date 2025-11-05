af113c06395950b147b2006f4789a0c81bf17b47
Isaiah Eusebio <isaiahc.eusebio@gmail.com>
2025-11-04 16:18:29 -0800

    set up routing with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import Home from './pages/Home.jsx';
+import CreateRecipe from './pages/CreateRecipe.jsx';
+import RecipeDetail from './pages/RecipeDetail.jsx';
+import EditRecipe from './pages/EditRecipe.jsx';
+import AllRecipes from './pages/AllRecipes.jsx';
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="retro">
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateRecipe />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/recipe/edit/:id" element={<EditRecipe />} />
+        <Route path="/all" element={<AllRecipes />} />
 
       </Routes>
     </div>
---
+++ frontend/src/components/RateLimitedUI.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ZapIcon } from 'lucide-react';
+
+const RateLimitedUI = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default RateLimitedUI;
